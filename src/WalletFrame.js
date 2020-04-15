1650c7b389f5f09d869b143eb45e5ea394a2752e
Jonathan Dunlap <jdunlap@outlook.com>
2020-04-15 10:59:42 -0700

    add cors warning

--- src/WalletFrame.js
+++ src/WalletFrame.js
@@ -5,28 +5,31 @@ export class FrameProvider {
         this._window = windowRef;
         this.pubkey = pubkey;
         this.account = pubkey;
+
+        this._handleIframeTask = this.handleIframeTask.bind(this);
     }
 
     async setup(iframRef, frameSrc) {
         if (!this.pubkey && !this.account) throw new Error("provide pub address");
+        if(!iframRef) throw new Error('frame reference is not set: setup(iframRef, frameSrc)');
 
         this.frameSrc = frameSrc;
         this.iframRef = iframRef;
 
-        this._window.addEventListener("message", this.handleIframeTask);
+        this._window.addEventListener("message", this._handleIframeTask);
 
         this.iframRef.src = frameSrc;
         return this.account;
     }
 
     stop() {
-        this._window.removeEventListener("message", this.handleIframeTask);
+        this._window.removeEventListener("message", this._handleIframeTask);
     }
 
     handleIframeTask(event) {
         const data = event.data;
 
-        console.log("handleIframeTask state.account", this.account);
+        // console.log("handleIframeTask state.account", this.account);
 
         if (data.jsonrpc) {
             handleMsg(data, this.account, this.iframRef); // state.account
@@ -79,5 +82,10 @@ const handleMsg = async(data, acct, refiFrame) => {
         data: response
     };
     console.log("responding", msg);
-    refiFrame.contentWindow.postMessage(msg, "*"); // TODO .current
+    
+    if(!refiFrame.contentWindow || !refiFrame.contentWindow.postMessage) {
+        console.log('refiFrame.contentWindow', refiFrame.contentWindow);
+        throw new Error('cannot access iFrame, check CORS');
+    }
+    else refiFrame.contentWindow.postMessage(msg, "*"); // TODO .current
 };
