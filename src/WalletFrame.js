be38314b1653eb660811a6229489da06f13aa373
Jonathan Dunlap <jdunlap@outlook.com>
2020-05-07 15:49:30 -0700

    add estimateGas, getLogs, getEtherPrice, eth_getTransactionCount. Fixed gasPrice.

--- src/WalletFrame.js
+++ src/WalletFrame.js
@@ -1,22 +1,22 @@
-import { toBN } from "eth-sdk";
-import { ethers } from "ethers";
-const BN = require("bn.js");
+import { toBN } from 'eth-sdk';
+import { ethers } from 'ethers';
+const BN = require('bn.js');
 // https://api.infura.io/v1/jsonrpc/mainnet
 export class FrameProvider {
   // pubkey - web wallet public key
   constructor(sdk, windowRef, pubkey) {
     if (!sdk) {
-      throw new Error("no sdk provided");
+      throw new Error('no sdk provided');
     }
     this.sdk = sdk;
     this._window = windowRef;
     this.pubkey = pubkey;
     this.account = pubkey;
-    this.provider = ethers.getDefaultProvider("homestead");
+    this.provider = ethers.getDefaultProvider('homestead');
     // this.sdk.state$.notification$.subscribe();
 
     this._handleIframeTask = this.handleIframeTask.bind(this);
-    console.log("WalletFrame 0.0.44");
+    console.log('WalletFrame 0.0.44');
   }
 
   // prompt: ()=>(msg, approve, reject)
@@ -28,7 +28,7 @@ export class FrameProvider {
     let prompt = this.prompt;
     if (!prompt)
       prompt = function (msg, approve, reject) {
-        var retVal = confirm("Do you want to continue? \nTransaction: " + msg);
+        var retVal = confirm('Do you want to continue? \nTransaction: ' + msg);
         if (retVal) approve();
         else reject();
       };
@@ -50,14 +50,13 @@ export class FrameProvider {
 
   async destroy() {
     this.frameSrc = null; // flag as destroyed
-    this._window.removeEventListener("message", this._handleIframeTask);
+    this._window.removeEventListener('message', this._handleIframeTask);
   }
 
   async setup(iframRef, frameSrc) {
-    if (!this.pubkey && !this.account) throw new Error("provide pub address");
-    if (!iframRef)
-      throw new Error("frame reference is not set: setup(iframRef, frameSrc)");
-    if (!frameSrc) throw new Error("no iFrame src");
+    if (!this.pubkey && !this.account) throw new Error('provide pub address');
+    if (!iframRef) throw new Error('frame reference is not set: setup(iframRef, frameSrc)');
+    if (!frameSrc) throw new Error('no iFrame src');
     if (this.iframRef) {
       this.destroy();
     }
@@ -67,15 +66,15 @@ export class FrameProvider {
 
     const p = new Promise((x) => {
       const onLoad = () => {
-        this.iframRef.removeEventListener("load", onLoad);
+        this.iframRef.removeEventListener('load', onLoad);
 
         if (!this.frameSrc) {
-          throw new Error("setup failed, already destroyed"); // destroyed
+          throw new Error('setup failed, already destroyed'); // destroyed
         }
-        this._window.addEventListener("message", this._handleIframeTask);
+        this._window.addEventListener('message', this._handleIframeTask);
         x();
       };
-      this.iframRef.addEventListener("load", onLoad);
+      this.iframRef.addEventListener('load', onLoad);
     });
     this.iframRef.src = frameSrc;
     return p;
@@ -96,11 +95,7 @@ export class FrameProvider {
   // Optional if needed to createAccount
   async createAccount(pubKey) {
     return await this.sdk.createAccount(pubKey).then((x) => {
-      console.log(
-        "x2 postcreateAccount",
-        x.address,
-        `x.deployed=${x.deployed}`
-      );
+      console.log('x2 postcreateAccount', x.address, `x.deployed=${x.deployed}`);
       this.account = x.address;
       return x;
     });
@@ -122,7 +117,7 @@ const convertBNProps = (obj) => {
     } else if (BN.isBN(obj[k])) {
       // PROBABLY DEAD PATH
       // console.log("v.BN", v._hex, obj[k].toString(16));
-      out[k] = "0x" + obj[k].toString(16);
+      out[k] = '0x' + obj[k].toString(16);
     } else if (v._hex) {
       // PROBABLY DEAD PATH
       // console.log("v._hex", v._hex, obj[k].toString(16));
@@ -135,14 +130,14 @@ const convertBNProps = (obj) => {
 
 let subIDHash = {};
 const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
-  if (!refiFrame) throw new Error("no refiFrame");
+  if (!refiFrame) throw new Error('no refiFrame');
   // const provider = window.ethereum;
   const method = data.method;
   const params = data.params; // TODO
   const jsonrpc = data.jsonrpc;
   const id = data.id;
   // console.log("state", state);
-  console.log("handleIframeTask", data);
+  console.log('handleIframeTask', data);
   let response = {
     jsonrpc: jsonrpc,
     id: data.id,
@@ -154,58 +149,60 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
 
   let options = {};
   let result = null;
+
+  const shortMethodName = method.replace('eth_', '');
   switch (method) {
-    case "request_funds":
+    case 'request_funds':
     // TODO
     // CUSTOM ABI
-    case "enable":
+    case 'enable':
       // Metamask specific rpc
       result = [acct];
       break;
-    case "eth_accounts":
+    case 'eth_accounts':
       result = [acct];
       break;
-    case "eth_getBalance":
+    case 'eth_getBalance':
       options = hasParams ? { address: params[0] } : {};
       const b = await sdk.getBalance(options);
       // console.log('getBalance', b);
 
-      result = "0x" + b.toString(16);
+      result = '0x' + b.toString(16);
       break;
-    case "eth_sign":
+    case 'eth_sign':
       if (!hasParams) {
-        throw new Error("eth_sign: no param provided");
+        throw new Error('eth_sign: no param provided');
       }
       // throw new Error('eth_sign not supported');
       const sig = await sdk.signMessage(params[0]);
       result = sig;
       break;
-    case "personal_sign":
+    case 'personal_sign':
       // params[2] is pw
       // TODO: cleanup
-      const msg = params[0] + (params[2] ? params[2] : "");
+      const msg = params[0] + (params[2] ? params[2] : '');
       const psig = await sdk.signMessage(msg);
       result = psig;
       break;
-    case "eth_sendRawTransaction":
-      throw new Error("eth_sendRawTransaction not supported");
+    case 'eth_sendRawTransaction':
+      throw new Error('eth_sendRawTransaction not supported');
       break;
-    case "eth_getTransactionByHash":
+    case 'eth_getTransactionByHash':
       const rTransHash = await _this.provider.getTransaction(param0);
-      console.log("eth_getTransactionByHash", rTransHash);
+      console.log('eth_getTransactionByHash', rTransHash);
       result = rTransHash;
       break;
-    case "eth_getBlockByHash":
-    case "eth_getBlockByNumber":
+    case 'eth_getBlockByHash':
+    case 'eth_getBlockByNumber':
       let getBlock = await _this.provider.getBlock(param0);
       getBlock = convertBNProps(getBlock);
 
-      console.log("eth_getBlockByNumber", getBlock, ",", getBlock.gasLimit);
+      console.log('eth_getBlockByNumber', getBlock, ',', getBlock.gasLimit);
 
       result = getBlock;
       break;
       break;
-    case "eth_getTransactionReceipt":
+    case 'eth_getTransactionReceipt':
       const interval = setInterval(async () => {
         let r2 = await _this.provider.getTransactionReceipt(param0);
 
@@ -214,7 +211,7 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
           //  r2.cumulativeGasUsed = "0x" + r2.cumulativeGasUsed.toString(16);
           // if (r2.gasUsed) r2.gasUsed = "0x" + r2.gasUsed.toString(16);
 
-          console.log("jd getTransactionReceipt polled");
+          console.log('jd getTransactionReceipt polled');
           sendMessage(
             {
               ...response,
@@ -226,20 +223,20 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
         }
       }, 2200);
       return;
-    case "eth_pendingTransactions":
-      throw new Error("eth_pendingTransactions not supported");
+    case 'eth_pendingTransactions':
+      throw new Error('eth_pendingTransactions not supported');
       break;
-    case "eth_unsubscribe":
-      console.warn("eth_unsubscribe unsupported");
+    case 'eth_unsubscribe':
+      console.warn('eth_unsubscribe unsupported');
       break;
-    case "eth_subscribe":
-      console.warn("eth_subscribe stubbed");
+    case 'eth_subscribe':
+      console.warn('eth_subscribe stubbed');
       // pubsub: https://github.com/ethereum/go-ethereum/wiki/RPC-PUB-SUB
-      if (param0 === "newHeads") {
-        const resultId = "0x" + id.toString(16);
+      if (param0 === 'newHeads') {
+        const resultId = '0x' + id.toString(16);
 
         if (subIDHash[id]) {
-          console.warn("already subscribed: " + param0);
+          console.warn('already subscribed: ' + param0);
           return;
         }
         subIDHash[id] = params;
@@ -261,7 +258,7 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
 
           sendMessage(
             {
-              method: "eth_subscription",
+              method: 'eth_subscription',
               params: {
                 result: {
                   ...gb,
@@ -272,43 +269,43 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
             refiFrame
           );
 
-          if (blockCount === 10) _this.provider.removeListener("block", f);
+          if (blockCount === 10) _this.provider.removeListener('block', f);
           // if(highestBlock===0) highestBlock = b;
           // highestBlock = b > highestBlock ? b : highestBlock;
         };
-        _this.provider.on("block", f);
+        _this.provider.on('block', f);
 
         result = resultId;
         break;
         // return;
       } else {
-        throw new Error("unsupported eth_subscribe method: " + data);
+        throw new Error('unsupported eth_subscribe method: ' + data);
       }
       break;
-    case "net_version":
+    case 'net_version':
       // Ethereum Mainnet
       // TODO: support Kovan
-      result = "1";
+      result = '1';
       break;
-    case "eth_call":
+    case 'eth_call':
       const rEthCall = await _this.provider.call(param0);
       result = rEthCall;
       break;
-    case "eth_sendTransaction":
+    case 'eth_sendTransaction':
       if (!hasParams) {
-        throw new Error("eth_sendTransaction: params provided");
+        throw new Error('eth_sendTransaction: params provided');
       }
       if (!param0.to) {
-        throw new Error("eth_sendTransaction: no To address");
+        throw new Error('eth_sendTransaction: no To address');
       }
       if (!param0.value && !param0.data) {
-        throw new Error("eth_sendTransaction: no value or data to invoke");
+        throw new Error('eth_sendTransaction: no value or data to invoke');
       }
 
       // For debugging only
       if (false) {
         // for testing
-        result = "0x0";
+        result = '0x0';
         if (true) break;
         return;
       }
@@ -326,7 +323,7 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
       const prettyMsg = JSON.stringify(optionsPretty, null, 2);
       const accepted = await _this._doPrompt(prettyMsg);
       if (!accepted) {
-        console.warn("user cancelled");
+        console.warn('user cancelled');
         return;
       }
 
@@ -341,14 +338,13 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
       const sub = sdk.state$.notification$.subscribe((x) => {
         if (!x || !x.payload) return;
 
-        if (x.type !== "RelayedTransactionUpdated") return;
+        if (x.type !== 'RelayedTransactionUpdated') return;
         if (x.payload.key === responseKey) {
-          console.warn("jd RelayedTransactionUpdated", x);
-          if (x.payload.state !== "Sending" && x.payload.state !== "Sent")
-            return; // Sent
+          console.warn('jd RelayedTransactionUpdated', x);
+          if (x.payload.state !== 'Sending' && x.payload.state !== 'Sent') return; // Sent
           //  && x.payload.hash
-          if (!x.payload.hash) x.payload.hash = "0x0";
-          console.warn("hash found!", x.payload);
+          if (!x.payload.hash) x.payload.hash = '0x0';
+          console.warn('hash found!', x.payload);
           const trHash = x.payload.hash;
           response.result = trHash;
           sendMessage(response, refiFrame);
@@ -362,17 +358,36 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
       // result = trHash; // "0x0";
       return;
     // break;
-    case "eth_gasPrice":
+    case 'eth_gasPrice':
       // To do: make accurate
       // result = "0x09184e72a000";
       result = await _this.provider.getGasPrice();
+      if (result.toHexString) result = result.toHexString();
       result = result.toString();
       break;
-    case "web3_clientVersion":
-      result = "3frame/0.0.11";
+
+    case 'web3_clientVersion':
+      result = '3frame/0.0.11';
+
+    case 'eth_estimateGas':
+      result = await _this.provider.perform(shortMethodName, { transaction: params });
+      break;
+
+    case 'eth_getLogs':
+      result = await _this.provider.perform(shortMethodName, { filter: param0 });
+      break;
+
+    case 'eth_getEtherPrice':
+      result = await _this.provider.perform(shortMethodName);
+      break;
+
+    case 'eth_getTransactionCount':
+      result = await _this.provider.perform(shortMethodName, { address: param0 });
+      break;
+
     default:
       // console.warn("non implemented event:", method, data);
-      throw new Error(method + " not supported");
+      throw new Error(method + ' not supported');
     // result = "";
   }
 
@@ -385,19 +400,19 @@ const handleMsg = async (data, acct, refiFrame, sdk, _this) => {
 
 function sendMessage(msg, refiFrame) {
   if (!refiFrame) {
-    throw new Error("null reference to refiFrame");
+    throw new Error('null reference to refiFrame');
   }
   const msgObj = {
-    jsonrpc: msg.jsonrpc || "2.0",
+    jsonrpc: msg.jsonrpc || '2.0',
     ...msg,
   };
-  console.log("responding", msgObj);
+  console.log('responding', msgObj);
 
   const ref = refiFrame.contentWindow || refiFrame.contentDocument;
   if (!ref) {
-    console.log("refiFrame.contentWindow", refiFrame.contentWindow);
-    throw new Error("cannot access iFrame, check CORS");
-  } else refiFrame.contentWindow.postMessage(msgObj, "*");
+    console.log('refiFrame.contentWindow', refiFrame.contentWindow);
+    throw new Error('cannot access iFrame, check CORS');
+  } else refiFrame.contentWindow.postMessage(msgObj, '*');
 }
 
 export default FrameProvider;
